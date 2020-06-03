04af0daabbf3e9b26b6c5cfb0ebc3f3da7a3e4e5
Jeff Klepees <jklepees@gmail.com>
2020-06-02 20:54:15 -0500

    myapp works to parse the json returned from crt.sh

--- myapp.py
+++ myapp.py
@@ -2,16 +2,18 @@
 
 import sys
 import json
-import urllib.request
-import urllib.parse
+import requests
 
 # Define my variables
 domain = ""
 subdomain = ""
-url = 'https://crt.sh//?q='
+url = 'https://crt.sh//?q=%.'
+url2 = '&output=json'
 
 # Take user input from the CLI. This will be used for the request to crt.sh
 domain = sys.argv[1]
 
-req = urllib.request.urlopen('url' + 'domain')
-print(req.read().decode('utf-8'))
+r = requests.get(url + domain + url2)
+jsondata = json.loads(r.text)
+for (key,value) in enumerate(jsondata):
+    print(value['name_value'])
